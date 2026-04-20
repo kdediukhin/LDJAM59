14e328296f93b7577cdb0c6cddf00e9ee593c5a9
kdediukhin-alhemija <kdeduhin@gmail.com>
2026-04-20 14:56:22 +0300

    level fix, placer rotation fix

--- LDJAM59/assets/scripts/EquipmentPlacer.ts
+++ LDJAM59/assets/scripts/EquipmentPlacer.ts
@@ -27,6 +27,8 @@ export class EquipmentPlacer extends Component {
     private _rotationOffset: Vec2 = null;
     private _uiNode: Node = null;
 
+    private _initalRotation: number | null = null;
+
     onEnable() {
 
         geometry.Plane.fromNormalAndPoint(this._groundPlane, new Vec3(0, 1, 0), Vec3.ZERO);
@@ -87,8 +89,10 @@ export class EquipmentPlacer extends Component {
         this._rotationOffset = null;
         this._movementOffset = null;
 
-         this._isActivePlacer = isOn;
+        this._isActivePlacer = isOn;
         this.outline.active = isOn;
+
+        this._initalRotation = isOn? this.node.eulerAngles.y : null;
     }
 
     // private onPurchaseDeny() {
@@ -155,9 +159,13 @@ export class EquipmentPlacer extends Component {
         const deltaX = currentPos.x - uiPos.x;//this._rotationOffset.x;
         const deltaY = currentPos.y - uiPos.y;//this._rotationOffset.y;
         // const delta = deltaX - deltaY;
-        const rotationY = this.node.eulerAngles.y - deltaY * 0.01;
-        this.node.setRotationFromEuler(0, rotationY, 0);
-        this._rotationOffset.set(currentPos.x, currentPos.y);
+        // const rotationY = this.node.eulerAngles.y - deltaY * 0.01;
+        if (this._initalRotation !== null) {
+            const rotationY = this._initalRotation - deltaY;
+            this.node.setRotationFromEuler(0, rotationY, 0);
+            this._rotationOffset.set(currentPos.x, currentPos.y);
+        }
+        
     }
 
 
